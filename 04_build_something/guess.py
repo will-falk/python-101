e9ef372b70990c3d68869eced404ce7e752b6426
will-sauer <will@heueso.com>
2025-12-30 12:03:53 -0500

    renaming, removing hyphens

---
+++ 04_build_something/guess.py
@@ -0,0 +1,18 @@
+#just going to roll my own guessing game
+#user has 5 tries to guess a number between 1 and 100
+#game gives feedback of whether each guess is above or below the number
+#after 5th fail, number is revealed
+#user can continue to press end to stop playing
+
+
+
+
+
+
+
+
+
+
+
+
+
